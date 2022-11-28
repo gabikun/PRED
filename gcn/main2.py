ba5f7cffd534ea75332603fa05b25b72224ed80d
Thomas Clouet <thomas.clouet1@etu.univ-nantes.fr>
2022-11-28 18:49:24 +0100

    :zap: Vertex 175 dim

--- gcn/main2.py
+++ gcn/main2.py
@@ -1,6 +1,8 @@
 import gcnModel
 import PRED.utils.molReader as molReader
+import numpy as np
 import torch.nn.functional as F
+
 
 molecule, featureMat, adjMat, reguMat = molReader.reader('C1CC1C2CC2')
 print("reguMat")
@@ -15,8 +17,13 @@ gcn_model = gcnModel.GCNNetwork(
     n_outputs=175,
     n_layers=4,
     hidden_sizes=[15, 20, 27, 36],
-    activation=F.selu,
+    activation=np.tanh,
     seed=100
 )
-
 print(gcn_model)
+
+y_pred = gcn_model.forward(reguMat, featureMat)
+print(y_pred)
+
+vertexF = y_pred.sum(axis=0)
+print(vertexF)
